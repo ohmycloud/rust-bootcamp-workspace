3c3dd0685cb12e65dabd7c319708b6fdb9ae4a60
ohmycloud <ohmycloudy@gmail.com>
2025-04-29 16:50:02 +0800

    add error example

--- ecosystem/src/main.rs
+++ ecosystem/src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use std::fs;
+
+use anyhow::Context;
+use ecosystem::MyError;
+
+fn fail_with_error() -> Result<(), MyError> {
+    Err(MyError::Custom("This is a custom error".to_string()))
+}
+
+fn main() -> Result<(), anyhow::Error> {
+    let filename = "test.raku";
+    let _fd =
+        fs::File::open(filename).with_context(|| format!("Failed to open file `{}`", filename))?;
+    fail_with_error()?;
+
+    Ok(())
 }
