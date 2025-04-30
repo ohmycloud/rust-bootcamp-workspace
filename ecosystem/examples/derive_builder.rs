1749d3b9cccc7389b900d5e084258c873e8acbbf
ohmycloud <ohmycloudy@gmail.com>
2025-05-01 07:54:51 +0800

    add a DateTime field

--- ecosystem/examples/derive_builder.rs
+++ ecosystem/examples/derive_builder.rs
@@ -1,4 +1,5 @@
 use anyhow::Result;
+use chrono::{DateTime, Utc};
 use derive_builder::Builder;
 
 #[derive(Debug, Builder)]
@@ -7,6 +8,8 @@ struct User {
     name: String,
     #[builder(setter(into, strip_option), default)]
     email: Option<String>,
+    #[builder(setter(custom))]
+    dob: DateTime<Utc>,
     #[builder(default = "20")]
     age: u32,
     #[builder(default = vec![], setter(each(name = "skill", into)))]
@@ -19,12 +22,24 @@ impl User {
     }
 }
 
+impl UserBuilder {
+    pub fn dob(&mut self, dob: &str) -> &mut Self {
+        self.dob = Some(
+            DateTime::parse_from_rfc3339(dob)
+                .unwrap()
+                .with_timezone(&Utc),
+        );
+        self
+    }
+}
+
 fn main() -> Result<()> {
     let user = User::build()
         .name("Alice")
         .skill("书法")
         .skill("音乐")
         .email("ohmycloudy@uk")
+        .dob("1994-07-05T08:00:00Z")
         .build()?;
     println!("{:?}", user);
 
