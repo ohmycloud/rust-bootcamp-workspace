a9d05286425cade1cfa5825dc3ed62c2be21f37a
ohmycloud <ohmycloudy@gmail.com>
2025-05-01 14:54:57 +0800

    impl Deserialize for a struct

--- ecosystem/examples/derive_serde.rs
+++ ecosystem/examples/derive_serde.rs
@@ -56,6 +56,62 @@ impl<'de> Visitor<'de> for UserVisitor {
 
         Ok(user)
     }
+
+    fn visit_map<A>(self, map: A) -> Result<User, A::Error>
+    where
+        A: serde::de::MapAccess<'de>,
+    {
+        let mut name = None;
+        let mut age = None;
+        let mut dob = None;
+        let mut skills = None;
+
+        let mut map = map;
+        while let Some(key) = map.next_key()? {
+            match key {
+                "name" => {
+                    if name.is_some() {
+                        return Err(serde::de::Error::duplicate_field("name"));
+                    }
+                    name = Some(map.next_value()?);
+                }
+                "age" => {
+                    if age.is_some() {
+                        return Err(serde::de::Error::duplicate_field("age"));
+                    }
+                    age = Some(map.next_value()?);
+                }
+                "dob" => {
+                    if dob.is_some() {
+                        return Err(serde::de::Error::duplicate_field("dob"));
+                    }
+                    dob = Some(map.next_value()?);
+                }
+                "skills" => {
+                    if skills.is_some() {
+                        return Err(serde::de::Error::duplicate_field("skills"));
+                    }
+                    skills = Some(map.next_value()?);
+                }
+                _ => {
+                    let _: serde::de::IgnoredAny = map.next_value()?;
+                }
+            }
+        }
+
+        let name = name.ok_or_else(|| serde::de::Error::missing_field("name"))?;
+        let age = age.ok_or_else(|| serde::de::Error::missing_field("age"))?;
+        let dob = dob.ok_or_else(|| serde::de::Error::missing_field("dob"))?;
+        let skills = skills.ok_or_else(|| serde::de::Error::missing_field("skills"))?;
+        let user = User {
+            name,
+            age,
+            dob,
+            skills,
+        };
+
+        Ok(user)
+    }
 }
 
 impl<'de> Deserialize<'de> for User {
