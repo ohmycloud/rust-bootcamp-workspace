fb1448f45720eb0b3a6b0bb7b0d5a3abb6aefb9d
ohmycloud <ohmycloudy@gmail.com>
2025-05-01 17:12:48 +0800

    serialize with serde_with

--- ecosystem/examples/user_serde.rs
+++ ecosystem/examples/user_serde.rs
@@ -1,4 +1,5 @@
 use anyhow::Result;
+use base64::{Engine, prelude::BASE64_URL_SAFE_NO_PAD};
 use chrono::{DateTime, Utc};
 use serde::{Deserialize, Serialize};
 
@@ -11,6 +12,8 @@ struct User {
     date_of_birth: DateTime<Utc>,
     skills: Vec<String>,
     state: WorkState,
+    #[serde(serialize_with = "b64_encode", deserialize_with = "b64_decode")]
+    data: Vec<u8>,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
@@ -22,6 +25,25 @@ enum WorkState {
     Terminated(DateTime<Utc>),
 }
 
+fn b64_encode<S>(data: &Vec<u8>, serializer: S) -> Result<S::Ok, S::Error>
+where
+    S: serde::Serializer,
+{
+    let encoded = BASE64_URL_SAFE_NO_PAD.encode(data);
+    serializer.serialize_str(&encoded)
+}
+
+fn b64_decode<'de, D>(deserializer: D) -> Result<Vec<u8>, D::Error>
+where
+    D: serde::Deserializer<'de>,
+{
+    let encoded = String::deserialize(deserializer)?;
+    let decoded = BASE64_URL_SAFE_NO_PAD
+        .decode(encoded.as_bytes())
+        .map_err(serde::de::Error::custom)?;
+    Ok(decoded)
+}
+
 fn main() -> Result<()> {
     let state = WorkState::Starting;
     let user = User {
@@ -30,6 +52,7 @@ fn main() -> Result<()> {
         date_of_birth: Utc::now(),
         skills: vec!["Rust".to_string(), "Python".to_string()],
         state,
+        data: vec![1, 2, 3, 4, 5],
     };
 
     let json = serde_json::to_string(&user)?;
