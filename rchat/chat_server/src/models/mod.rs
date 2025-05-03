7fcd278751df5ef61d0fdb4912fb19bb4a240a10
ohmycloud <ohmycloudy@gmail.com>
2025-05-03 21:58:26 +0800

    test jwt token

--- rchat/chat_server/src/models/mod.rs
+++ rchat/chat_server/src/models/mod.rs
@@ -4,7 +4,7 @@ use chrono::{DateTime, Utc};
 use serde::{Deserialize, Serialize};
 use sqlx::FromRow;
 
-#[derive(Debug, Clone, FromRow, Serialize, Deserialize)]
+#[derive(Debug, Clone, FromRow, Serialize, Deserialize, PartialEq)]
 pub struct User {
     pub id: i64,
     pub fullname: String,
@@ -13,4 +13,17 @@ pub struct User {
     #[serde(skip)]
     pub password_hash: Option<String>,
     pub created_at: DateTime<Utc>,
+}
+
+#[cfg(test)]
+impl User {
+    pub fn new(id: i64, fullname: &str, email: &str) -> Self {
+        User {
+            id,
+            fullname: fullname.to_string(),
+            email: email.to_string(),
+            password_hash: None,
+            created_at: Utc::now(),
+        }
+    }
 }
--- rchat/chat_server/src/utils/jwt.rs
+++ rchat/chat_server/src/utils/jwt.rs
@@ -15,10 +15,10 @@ impl EncodingKey {
         Ok(Self(key))
     }
 
-    pub fn sign(user: User, key: &EncodingKey) -> Result<String, AppError> {
-        let mut claims = Claims::with_custom_claims(user, Duration::from_secs(JWT_DURATION));
+    pub fn sign(&self, user: impl Into<User>) -> Result<String, AppError> {
+        let claims = Claims::with_custom_claims(user.into(), Duration::from_secs(JWT_DURATION));
         let claims = claims.with_issuer(JWT_ISSUER).with_audience(JWT_AUDIENCE);
-        Ok(key.sign(claims)?)
+        Ok(self.0.sign(claims)?)
     }
 }
 
@@ -28,33 +28,39 @@ impl DecodingKey {
         Ok(Self(key))
     }
 
-    pub fn verify(token: &str, key: &DecodingKey) -> Result<User, AppError> {
+    pub fn verify(&self, token: &str) -> Result<User, AppError> {
         let mut opts = VerificationOptions::default();
         opts.allowed_issuers = Some(HashSet::from_strings(&[JWT_ISSUER]));
         opts.allowed_audiences = Some(HashSet::from_strings(&[JWT_AUDIENCE]));
 
-        let claims = key.verify_token::<User>(token, Some(opts))?;
+        let claims = self.0.verify_token::<User>(token, Some(opts))?;
         Ok(claims.custom)
-    }
-}
-
-impl Deref for EncodingKey {
-    type Target = Ed25519KeyPair;
-
-    fn deref(&self) -> &Self::Target {
-        &self.0
-    }
-}
-
-impl Deref for DecodingKey {
-    type Target = Ed25519PublicKey;
-
-    fn deref(&self) -> &Self::Target {
-        &self.0
     }
 }
 
 pub fn generate_token(user: User, key: &EncodingKey) -> Result<String, AppError> {
     let claims = Claims::with_custom_claims(user, Duration::from_secs(JWT_DURATION));
-    Ok(key.sign(claims)?)
+    Ok(key.0.sign(claims)?)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[tokio::test]
+    async fn jwt_sign_verify_should_work() -> anyhow::Result<()> {
+        let encoding_pem = include_str!("../../fixtures/encoding.pem");
+        let decoding_pem = include_str!("../../fixtures/decoding.pem");
+        let ek = EncodingKey::load(encoding_pem)?;
+        let dk = DecodingKey::load(decoding_pem)?;
+
+        let user = User::new(1, "ohmycloudy", "ohmycloudy@uk");
+        let token = ek.sign(user.clone())?;
+        let decoded_user: User = dk.verify(&token)?;
+        
+        assert_eq!(user, decoded_user);
+
+        Ok(())
+
+    }
 }
